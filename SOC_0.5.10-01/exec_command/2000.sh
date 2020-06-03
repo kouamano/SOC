3e7d47281a186e230c57b9cf1efe2898ca217585
kouamano <amano.au1@gmail.com>
2020-06-03 12:45:50 +0900

    move

--- SOC_0.5.10-01/exec_command/2000.sh
+++ SOC_0.5.10-01/exec_command/2000.sh
@@ -1,5 +1,1 @@
-#1 hyperthreading:On
-time soc-lm sf=../test-data/test_matrix_2000.sv cf=../test-data/test_matrix_2000.cv.20.C > /dev/null
-#real	0m0.299s
-#user	0m0.293s
-#sys	0m0.006s
+time /home/kamano/gitsrc/SOC/SOC_0.5.10-01/soc-lm sf=/home/kamano/gitsrc/SOC/SOC_0.5.10-01/test-data/test_matrix_2000.sv cf=/home/kamano/gitsrc/SOC/SOC_0.5.10-01/test-data/test_matrix_2000.cv.20.C > soc.mini.out
