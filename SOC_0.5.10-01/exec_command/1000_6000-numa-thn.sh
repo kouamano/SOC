c682e586f94057c6d05da4132aef00940c4e33d2
kouamano <amano.au1@gmail.com>
2023-08-15 21:13:58 +0900

    rev

--- SOC_0.5.10-01/exec_command/1000_6000-numa-thn.sh
+++ SOC_0.5.10-01/exec_command/1000_6000-numa-thn.sh
@@ -1,2 +1,2 @@
 export OMP_NUM_THREADS=8
-time numactl --localalloc ../../bin/soc-lm sf=../test-data/t-1000_6000.sv cf=../test-data/t-1000_6000.D200.cv loop=100 rate=0.95 lf=../test-data/t-1000_6000.D200.cv.pt.log
+time numactl --localalloc ../soc-lm sf=../test-data/t-1000_6000.sv cf=../test-data/t-1000_6000.D200.cv loop=100 rate=0.95 lf=../test-data/t-1000_6000.D200.cv.pt.log
